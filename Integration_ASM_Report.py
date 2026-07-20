c7e94cf8f716be6941146b1d084deec5a1cc1cb8
Justin Schuessler <justin.schuessler@sedara.co>
2026-07-20 11:55:49 -0400

    Added Excel report generation

--- Integration_ASM_Report.py
+++ Integration_ASM_Report.py
@@ -1,6 +1,7 @@
 import sys
 import json
-import datetime
+from datetime import date, datetime
+
 import requests
 from requests.auth import HTTPBasicAuth
 import subprocess
@@ -18,13 +19,22 @@ if __name__ == "__main__":
     all_integrations = {}
     for tenant_id in active_tenants_list.keys():
         customer_integrations = Useful_ASM_functions.get_active_integrations(headers=headers, tenant_id=tenant_id)
+        print(active_tenants_list.get(tenant_id))
         all_integrations[tenant_id] = customer_integrations
         all_integrations[tenant_id]["name"] = active_tenants_list.get(tenant_id)
 
     data = all_integrations
     path = Useful_ASM_functions.get_asm_out_path()
+    xl_save_path = path + str(date.today()) + "ASM_Integrations_Health_Report.xlsx"
+    Fullpath = path + "_ASM_all_integrations_all_tenants.json"
 
-    Fullpath = path + "ASM_all_integrations_all_tenants.json"
+    try:
+        Useful_ASM_functions.out_to_excel(data=data, xl_save_path=xl_save_path)
+
+    except:
+        xl_save_path_2 = path + str(datetime.now().strftime("%Y-%-m-%-d_%H-%M")) + "_ASM_Integrations_Health_Report.xlsx"
+        Useful_ASM_functions.out_to_excel(data=data, xl_save_path=xl_save_path_2)
+
     f = open(Fullpath, "w")
     sys.stdout = f
     output = json.dumps(data, indent=4, )
--- Useful_ASM_functions.py
+++ Useful_ASM_functions.py
@@ -5,6 +5,7 @@ import requests
 from requests.auth import HTTPBasicAuth
 import subprocess
 import get_1password_field
+from openpyxl import Workbook
 
 Base_URL = "https://platform.sedaraapis.com/"
 asm_out_path = "C:/Users/justin.schuessler/Documents/API_Output/Sedara_ASM/"
@@ -34,7 +35,7 @@ def get_active_integrations(headers, tenant_id, params=None):
         name = integration["attributes"].get("title")
         configured = integration["meta"].get("configured")
         description = integration["attributes"].get("description")
-        integration_id = integration["attributes"].get("id")
+        integration_id = integration.get("id")
         integration_out_dict["name"] = name
         integration_out_dict["description"] = description
         if configured > 0:
@@ -131,6 +132,39 @@ def list_all_asm_collectors_health(headers, params=None):
     response.raise_for_status()
 
     return response.json()
+
+
+def out_to_excel(data, xl_save_path):
+    wb = Workbook()
+    ws = wb.active
+    row = 2
+
+    ws["A1"] = "Tenant ID"
+    ws["B1"] = "Tenant Name"
+    ws["C1"] = "Integration Name"
+    ws["D1"] = "Integration ID"
+    ws["E1"] = "Healthy"
+    ws["F1"] = "State"
+    ws["G1"] = "Status List"
+    ws["H1"] = "Last Seen"
+    data_keys = data.keys()
+
+    for key in data_keys:
+        configed_dict = data[key].get("configured_integrations_list")
+        if len(configed_dict) > 0:
+            for configed_integration in data[key].get("configured_integrations_list").keys():
+                ws.cell(row=row, column=1).value = key
+                ws.cell(row=row, column=2).value = data[key].get("name")
+                ws.cell(row=row, column=3).value = data[key]["configured_integrations_list"][configed_integration].get("name")
+                ws.cell(row=row, column=4).value = configed_integration
+                ws.cell(row=row, column=5).value = data[key]["configured_integrations_list"][configed_integration].get("healthy")
+                ws.cell(row=row, column=6).value = data[key]["configured_integrations_list"][configed_integration].get("state")
+                status_string = ", ".join(data[key]["configured_integrations_list"][configed_integration].get("status_list"))
+                ws.cell(row=row, column=7).value = status_string
+                ws.cell(row=row, column=8).value = data[key]["configured_integrations_list"][configed_integration].get("last_seen")
+                row += 1
+
+    wb.save(xl_save_path)
 
 
 def get_base_url():
